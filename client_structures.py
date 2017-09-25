b1ed2500028a4d6563d133848970a92f5a785e75
lars <twobraids@gmail.com>
2017-09-25 10:43:40 -0700

    style and commenting

--- client_structures.py
+++ client_structures.py
@@ -1,11 +1,34 @@
 from numpy.random import (
-    laplace
+    random,
+    laplace,
+    choice,
 )
 
 from in_memory_structures import (
     URLCounter,
 )
 
+
+def local_alg(config, head_list, local_query_url_iter):
+    """to be used in testing as in production, it will be executed by the client.  It should, therefore,
+    be written in Javascript or Rust"""
+    kappa = head_list.count
+    tau = (
+        (exp(config.epsilon_prime_q) + (config.delta_prime_q / 2.0) * (kappa - 1))
+        /
+        (exp(config.epsilon_prime_q) + kappa - 1)
+    )
+    for a_query, a_url in local_query_url_iter():
+        if a_query not in head_list:
+            a_query = '*'
+        if a_url not in head_list[a_query]:
+            a_url = '*'
+
+        if random() <= (1 - tau):
+            # there is confusing on the significance of a database structure has only unqiue <q, u> pairs
+            # or duplicates.  Some code clearly allows duplicates.  the definiton of |D| is for unique or
+            # with duplicates?
+            a_query = choice(head_list.keys())
 
 class URLStatsForClient(URLCounter):
     def __init__(self, config, count=0):
