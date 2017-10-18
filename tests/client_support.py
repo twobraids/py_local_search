7aba8ebcd7f62d2786c76555c5d0379ca40569de
lars <twobraids@gmail.com>
2017-10-18 16:38:03 -0700

    reorganization of tests

---
+++ tests/client_support.py
@@ -0,0 +1,39 @@
+from math import (
+    exp
+)
+
+from numpy.random import (
+    random,
+    choice,
+)
+
+def local_alg(config, head_list, local_query_url_iter):
+    """to be used in testing as in production, it will be executed by the client.  It should, therefore,
+    be written in Javascript or, even better, Rust"""
+    kappa = head_list.count
+    tau = (
+        (exp(config.epsilon_prime_q) + (config.delta_prime_q / 2.0) * (kappa - 1))
+        /
+        (exp(config.epsilon_prime_q) + kappa - 1)
+    )
+    for a_query, a_url in local_query_url_iter():
+        if a_query not in head_list:
+            a_query = '*'
+        if a_url not in head_list[a_query]:
+            a_url = '*'
+
+        if random() <= (1 - tau):
+            # there is confusion on the significance of a database structure has only unqiue <q, u> pairs
+            # or duplicates.  Some code clearly allows duplicates.  the definiton of |D| is for unique or
+            # with duplicates?
+            alt_query = choice(head_list.keys())
+            alt_url = choice(head_list[alt_query].keys())
+            yield alt_query, alt_url
+            continue
+
+        if random() <= (1 - head_list[a_query].tau):
+            alt_url = head_list[a_query].keys()
+            yield a_query, alt_url
+            continue
+
+        yield a_query, a_url
---
+++ tests/synthetic_data.py
@@ -0,0 +1,55 @@
+from functools import partial
+
+def load_synthetic_data_set(data_set, db):
+    for q, u, c in data_set:
+        for i in range(c):
+            db.add((q, u))
+    return db
+
+standard_constants = {
+    "epsilon": 4.0,
+    "delta": 0.000001,
+    "m_o": 10,
+    "head_list_db.m": 5,
+}
+
+tiny_set = [
+    ('q1', 'q1u1', 10),
+    ('q2', 'q2u1', 10),
+    ('q2', 'q2u2', 20),
+    ('q3', 'q3u1', 30),
+    ('q4', 'q4u1', 99),
+    ('q4', 'q4u2', 99),
+    ('q5', 'q5u1', 10),
+    ('q5', 'q5u2', 20),
+    ('q6', 'q6u1', 99),
+    ('q6', 'q6u2', 10),
+    ('q7', 'q7u1', 99),
+    ('q7', 'q7u2', 99),
+]
+load_tiny_data = partial(load_synthetic_data_set, tiny_set)
+
+small_set = [
+    ('q1', 'q1u1', 10),
+    ('q2', 'q2u1', 10),
+    ('q2', 'q2u2', 20),
+    ('q3', 'q3u1', 30),
+    ('q4', 'q4u1', 100),
+    ('q4', 'q4u2', 100),
+    ('q5', 'q5u1', 10),
+    ('q5', 'q5u2', 20),
+    ('q6', 'q6u1', 100),
+    ('q6', 'q6u2', 10),
+    ('q7', 'q7u1', 100),
+    ('q7', 'q7u2', 100),
+    ('q8', 'q8u1', 50),
+    ('q8', 'q8u2', 50),
+    ('q8', 'q8u3', 50),
+    ('q8', 'q8u4', 50),
+    ('q8', 'q8u5', 50),
+    ('q8', 'q8u6', 50),
+    ('q8', 'q8u7', 45),
+    ('q8', 'q8u8', 45),
+]
+load_small_data = partial(load_synthetic_data_set, small_set)
+
